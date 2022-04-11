f1d21f745f6bb083edb92ef5c53aa32831f51266
molchanov.v <molchanov.v@seleclet.ru>
2022-04-11 22:53:28 +0300

    add http service

---
+++ src/service/http.service.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+import configFile from "../config.json";
+
+const http = axios.create({
+    baseURL: configFile.apiEndpoint
+});
+
+function transformData(data) {
+    return data && !data._id
+        ? Object.keys(data).map((key) => ({ ...data[key] }))
+        : data;
+}
+
+http.interceptors.response.use(
+    (res) => {
+        if (configFile.isFireBase) {
+            res.data = { content: transformData(res.data) };
+        }
+        return res;
+    },
+    (error) => {
+        const expectedErrors =
+            error.response &&
+            error.response.status >= 400 &&
+            error.response.status < 500;
+        if (!expectedErrors) {
+            console.log(error);
+            // Вывод ошибки запроса на публичную часть
+        }
+        return Promise.reject(error);
+    }
+);
+
+const httpService = {
+    get: http.get,
+    post: http.post,
+    put: http.put,
+    delete: http.delete,
+    patch: http.patch
+};
+
+export default httpService;
