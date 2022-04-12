bfa1acc89e179c03dde9ac21b3d12fefe8b07328
molchanov.v <molchanov.v@seleclet.ru>
2022-04-12 18:50:41 +0300

    feat(loader) add colors and category loader

--- src/components/ui/hoc/app.Loader.jsx
+++ src/components/ui/hoc/app.Loader.jsx
@@ -2,11 +2,15 @@ import { useEffect } from "react";
 import PropTypes from "prop-types";
 import { useDispatch } from "react-redux";
 import { loadProductsList } from "../../../store/products";
+import { loadColorsList } from "../../../store/color";
+import { loadCategoryList } from "../../../store/category";
 
 const AppLoader = ({ children }) => {
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(loadProductsList());
+        dispatch(loadColorsList());
+        dispatch(loadCategoryList());
     }, []);
     return children;
 };
