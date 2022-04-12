214b816d013b1966ef2f13f7ac5c9e31379261e7
molchanov.v <molchanov.v@seleclet.ru>
2022-04-13 00:43:14 +0300

    feat(loader) add basket loader

--- src/components/ui/hoc/app.Loader.jsx
+++ src/components/ui/hoc/app.Loader.jsx
@@ -4,6 +4,7 @@ import { useDispatch } from "react-redux";
 import { loadProductsList } from "../../../store/products";
 import { loadColorsList } from "../../../store/color";
 import { loadCategoryList } from "../../../store/category";
+import { loadBasketList } from "../../../store/basket";
 
 const AppLoader = ({ children }) => {
     const dispatch = useDispatch();
@@ -11,6 +12,7 @@ const AppLoader = ({ children }) => {
         dispatch(loadProductsList());
         dispatch(loadColorsList());
         dispatch(loadCategoryList());
+        dispatch(loadBasketList());
     }, []);
     return children;
 };
