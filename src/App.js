c33270600cf9d05d5536b9fdada576b61ef76db0
molchanov.v <molchanov.v@seleclet.ru>
2022-04-13 18:30:18 +0300

    feat(router) add route basket

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "./App.css";
 import Main from "./layouts/main";
 import Products from "./layouts/products";
 import AppLoader from "./components/ui/hoc/app.Loader";
+import Basket from "./layouts/basket";
 
 function App() {
     return (
         <AppLoader>
             <Switch>
                 <Route path="/products" component={Products} />
+                <Route path="/basket" component={Basket} />
                 <Route path="/" exact component={Main} />
                 <Redirect to="/" />
             </Switch>
