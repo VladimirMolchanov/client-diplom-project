d1dfb9f1412ad5f016fc2a7f39d4389584abbe88
molchanov.v <molchanov.v@seleclet.ru>
2022-04-13 21:43:23 +0300

    feat(basket) add view basket

--- src/components/pages/basket.jsx
+++ src/components/pages/basket.jsx
@@ -1,12 +1,103 @@
 import React from "react";
+import "./basket.css";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteBasketProduct, getBasket } from "../../store/basket";
 
 const Basket = () => {
+    const dispatch = useDispatch();
+    const basket = useSelector(getBasket());
+    const totalPrice = basket.reduce(
+        (price, product) => price + product.price * product.count,
+        0
+    );
+    const handleDelete = (id) => {
+        dispatch(deleteBasketProduct(id));
+    };
+
     return (
         <section className="basket section">
             <div className="basket__container container">
                 <h2 className="section__title section__title-gradient text-left">
                     Корзина
                 </h2>
+
+                {basket.length > 0 ? (
+                    <div>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Product</th>
+                                    <th>Quantity</th>
+                                    <th>Subtotal</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {basket &&
+                                    basket.map((product) => (
+                                        <tr key={product._id}>
+                                            <td>
+                                                <div className="cart__info">
+                                                    <img
+                                                        src="http://localhost:8080/assets/image/BeatsStudio3Wireless_Blue.png"
+                                                        alt=""
+                                                    />
+                                                    <div>
+                                                        <p>{product.name}</p>
+                                                        <small>
+                                                            Price: $
+                                                            {product.price}
+                                                        </small>
+                                                        <br />
+                                                        <button
+                                                            type="button"
+                                                            onClick={() => {
+                                                                handleDelete(
+                                                                    product._id
+                                                                );
+                                                            }}
+                                                            className="button button--flex"
+                                                        >
+                                                            Удалить
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <input
+                                                    type="number"
+                                                    value={product.count}
+                                                />
+                                            </td>
+                                            <td>
+                                                ${product.count * product.price}
+                                            </td>
+                                        </tr>
+                                    ))}
+                            </tbody>
+                        </table>
+
+                        <div className="total__price">
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        <td>SubTotal</td>
+                                        <td>$200.00</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Tax</td>
+                                        <td>$35.00</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Total</td>
+                                        <td>${totalPrice}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                ) : (
+                    "Empty cart"
+                )}
             </div>
         </section>
     );
