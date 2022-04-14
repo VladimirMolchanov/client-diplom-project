512747a0ba408fc716a9d14d88df32dbfaa44be8
molchanov.v <molchanov.v@seleclet.ru>
2022-04-14 23:25:23 +0300

    feat(register)

--- src/components/ui/registerForm.jsx
+++ src/components/ui/registerForm.jsx
@@ -1,9 +1,116 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import TextField from "../common/form/textField";
+import validator from "../../utils/validator";
 
 const RegisterForm = () => {
+    const [data, setData] = useState({
+        email: "",
+        password: "",
+        name: ""
+    });
+    const [error, setError] = useState({});
+
+    const handleChange = (target) => {
+        if (target) {
+            setData((prevState) => ({
+                ...prevState,
+                [target.name]: target.value
+            }));
+        }
+    };
+
+    const validatorConfig = {
+        email: {
+            isRequired: {
+                message: "Электронная почта обязательна для заполнения"
+            },
+            isEmail: {
+                message: "Email введен некорректно"
+            }
+        },
+        name: {
+            isRequired: {
+                message: "Имя обязательно для заполнения"
+            },
+            min: {
+                message: "Имя должен состоять минимум из 3 символов",
+                value: 3
+            }
+        },
+        password: {
+            isRequired: {
+                message: "Пароль обязательна для заполнения"
+            },
+            isCapitalSymbol: {
+                message: "Пароль должен содержать хотя бы одну заглавную букву"
+            },
+            isContainDigit: {
+                message: "Пароль должен содержать хотя бы одно число"
+            },
+            min: {
+                message: "Пароль должен состоять минимум из 8 символов",
+                value: 8
+            }
+        }
+    };
+
+    const validate = () => {
+        const error = validator(data, validatorConfig);
+        setError(error);
+        return Object.keys(error).length === 0;
+    };
+    const isValid = Object.keys(error).length === 0;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const isValid = validate();
+        if (!isValid) return;
+        const newData = {
+            ...data
+        };
+
+        console.log(newData);
+    };
+
+    useEffect(() => {
+        validate();
+    }, [data]);
+
     return (
         <div>
-            <h1>Register</h1>
+            <form onSubmit={handleSubmit}>
+                <h1>Регистрация</h1>
+                <TextField
+                    label="Имя"
+                    type="text"
+                    name="name"
+                    value={data.name}
+                    onChange={handleChange}
+                    error={error.name}
+                />
+                <TextField
+                    label="Электронная почта"
+                    name="email"
+                    value={data.email}
+                    onChange={handleChange}
+                    error={error.email}
+                />
+                <TextField
+                    label="Пароль"
+                    type="password"
+                    name="password"
+                    value={data.password}
+                    onChange={handleChange}
+                    error={error.password}
+                />
+                <button
+                    type="submit"
+                    disabled={!isValid}
+                    className="btn btn-primary w-100 mx-auto"
+                >
+                    Зарегистрироваться
+                </button>
+            </form>
         </div>
     );
 };
