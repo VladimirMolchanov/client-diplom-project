76b89907cdc95f1d56370d265b02edfb6c6a10f2
molchanov.v <molchanov.v@seleclet.ru>
2022-04-16 15:10:24 +0300

    feat(service) add interceptor request

--- src/service/http.service.js
+++ src/service/http.service.js
@@ -1,21 +1,39 @@
 import axios from "axios";
 import configFile from "../config.json";
+import localStorageService from "./localStorageService";
+import authService from "./auth.service";
 
 const http = axios.create({
     baseURL: configFile.apiEndpoint
 });
 
-function transformData(data) {
-    return data && !data._id
-        ? Object.keys(data).map((key) => ({ ...data[key] }))
-        : data;
-}
+http.interceptors.request.use(async function (config) {
+    const expiresDate = localStorageService.getTokenExpiresDate();
+    const refreshToken = localStorageService.getRefreshToken();
+
+    if (refreshToken && expiresDate < Date.now()) {
+        const { data } = await authService.refresh();
+        localStorageService.setTokens({
+            refreshToken: data.refreshToken,
+            accessToken: data.accessToken,
+            expiresIn: data.expiresIn,
+            userId: data.userId
+        });
+    }
+    const accessToken = localStorageService.getAccessToken();
+    if (accessToken) {
+        config.headers = {
+            ...config.headers,
+            Authorization: `Bearer ${accessToken}`
+        };
+    }
+
+    return config;
+});
 
 http.interceptors.response.use(
     (res) => {
-        if (configFile.isFireBase) {
-            res.data = { content: transformData(res.data) };
-        }
+        res.data = { content: res.data };
         return res;
     },
     (error) => {
