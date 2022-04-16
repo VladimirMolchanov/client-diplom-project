61ad5efa02f8e8ac28151d3900e5090d9b4c8478
molchanov.v <molchanov.v@seleclet.ru>
2022-04-16 15:37:24 +0300

    feat(router) add router admin and logout

--- src/App.js
+++ src/App.js
@@ -6,14 +6,19 @@ import Products from "./layouts/products";
 import AppLoader from "./components/ui/hoc/app.Loader";
 import Basket from "./layouts/basket";
 import Login from "./layouts/login";
+import ProtectedRoute from "./components/common/protectedRoute";
+import Admin from "./layouts/admin";
+import LogOut from "./layouts/logOut";
 
 function App() {
     return (
         <AppLoader>
             <Switch>
+                <ProtectedRoute path="/admin/" component={Admin} />
                 <Route path="/products" component={Products} />
                 <Route path="/basket" component={Basket} />
                 <Route path="/login/:type?" component={Login} />
+                <Route path="/logout" component={LogOut} />
                 <Route path="/" exact component={Main} />
                 <Redirect to="/" />
             </Switch>
